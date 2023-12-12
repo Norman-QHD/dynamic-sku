13cf8fcad270583d38dce7457ac94c9b5c34a902
Donald Norman <125552898+Norman-QHD@users.noreply.github.com>
2023-12-12 13:42:20 +0000

    .

--- index.ts
+++ index.ts
@@ -1,27 +1,3 @@
-
-//定义格式1的数据类型
-type Format1 = {
-    name: string,
-    values: string
-}
-
-//定义格式2的数据类型
-type Format2 = {
-    name: string,
-    value: {
-        specName: string
-    }[],
-    child: {
-        value: string,
-        label: string
-    }[]
-}[]
-
-//定义格式3的数据类型
-type Format3 = {
-    [key: string]: string | number
-}[]
-
 //树节点类型.
 type TreeNode = {
     //唯一标识,在递归时按照处理顺序递增.
@@ -38,14 +14,14 @@ type TreeNode = {
     level: number,
     //当前节点下的子节点集合.
     children: TreeNode[]
-}
-//加载数据
-const data: Format1[] = require('./format1.json')
-
-//定义一个全局变量,用于记录当前处理的节点的id,在递归时递增.
-let id = 0
-//定义root节点
-const root: TreeNode = {
+  }
+  //加载数据
+  import data from './format1.json'
+  
+  //定义一个全局变量,用于记录当前处理的节点的id,在递归时递增.
+  let id:number = 0
+  //定义root节点
+  const root: TreeNode = {
     id: id++,
     name: 'root',
     fullName: 'root',
@@ -53,32 +29,39 @@ const root: TreeNode = {
     fullValue: 'root',
     level: 0,
     children: []
-}
-
-//每个层级的节点集合,key是数字,value是tree node的集合
-let levelMap: { [key: number]: TreeNode[] } = {}
-levelMap[0] = [root]
-/*
-data:
-{
-  name:"自选小料,温度,甜度",
-  values:"椰果,红豆,奥利奥碎;热,常温,多冰;正常糖,七分糖,三分糖"
-}
-
-* */
-
-//从data中提取name的list,组装成name:[value1,value2,value3]的形式
-//第一步先把values提取出来变成一个数组
-let values:string = data.values.split(';')
-//第二步把name和values组装成一个对象
-let obj = {}
-data.name.split(',').forEach((name,index)=>{
+  }
+  
+  //每个层级的节点集合,key是数字,value是tree node的集合
+  let levelMap: { [key: number]: TreeNode[] } = {}
+  levelMap[0] = [root]
+  /*
+  data:
+  {
+    name:"自选小料,温度,甜度",
+    values:"椰果,红豆,奥利奥碎;热,常温,多冰;正常糖,七分糖,三分糖"
+  }
+  
+  * */
+  
+  //从data中提取name的list,组装成name:[value1,value2,value3]的形式
+  
+  
+  const startTimestamp = new Date().getTime()
+  
+  
+  
+  //第一步先把values提取出来变成一个数组
+  let values:string[] = data.values.split(';')
+  //第二步把name和values组装成一个对象
+  let obj: {[key:string]:string[]} = {}
+  data.name.split(',').forEach((name,index)=>{
     obj[name] = values[index].split(',')
-})
-
-//对于如上数据,节点数量应该为3*3*3=27个
-let keys = Object.keys(obj)
-for (let i = 0; i <keys.length ; i++) {
+  })
+  
+  
+  //对于如上数据,节点数量应该为3*3*3=27个
+  let keys = Object.keys(obj)
+  for (let i = 0; i <keys.length ; i++) {
     let currentKey = keys[i]
     let currentValues = obj[currentKey]
     let currentLevel = i + 1
@@ -86,24 +69,44 @@ for (let i = 0; i <keys.length ; i++) {
     let parentNodes = levelMap[parentLevel]
     let currentNodes: TreeNode[] = []
     parentNodes.forEach((parentNode) => {
-            currentValues.forEach((value) => {
-                let currentNode: TreeNode = {
-                    id: id++,
-                    name: currentKey,
-                    fullName: parentNode.fullName + ' ' + currentKey,
-                    value: value,
-                    fullValue: parentNode.fullValue + ' ' + value,
-                    level: currentLevel,
-                    children: []
-                }
-                parentNode.children.push(currentNode)
-                currentNodes.push(currentNode)
-            })
+          currentValues.forEach((value) => {
+            let currentNode: TreeNode = {
+              id: id++,
+              name: currentKey,
+              fullName: parentNode.fullName + ' ' + currentKey,
+              value: value,
+              fullValue: parentNode.fullValue + ' ' + value,
+              level: currentLevel,
+              children: []
+            }
+            parentNode.children.push(currentNode)
+            currentNodes.push(currentNode)
+          })
         }
     )
     levelMap[currentLevel] = currentNodes
-}
+  }
+  
+  
+  
+  const endTimestamp = new Date().getTime()
+  
+  const cost = endTimestamp - startTimestamp
 
-//打印结果
-console.log('树:', JSON.stringify(root, null, 2))
-console.log('节点数量:', id)
+  
+  //打印结果
+  console.log('树:', JSON.stringify(root, null, 2))
+  console.log('节点数量:', id)
+  //输出map
+  const mapKeys= Object.keys(levelMap)
+  for (let i = 0; i < mapKeys.length; i++) {
+    let key = mapKeys[i]
+    console.log('level:', key, 'nodes:', levelMap[Number(key)].length)
+  }
+  //输出最后一层的节点
+  for (let i = 0; i < levelMap[mapKeys.length-1].length; i++) {
+    const current = levelMap[mapKeys.length-1][i]
+    console.log('树目录名/列名',current.fullName,'树值/格子值',current.fullValue, 'id:',current.id,'列号:',current.level,'行号:',i)
+  }
+  
+  console.log('耗时:', cost, 'ms')
